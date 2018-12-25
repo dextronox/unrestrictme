251c432def98946ac1b57a51bfe2202b96be3d2a
dex <dextronox@users.noreply.github.com>
2018-12-25 09:14:20 +0800

    Supports new error

--- src/error/index.js
+++ src/error/index.js
@@ -2,11 +2,24 @@ const {remote, ipcRenderer} = require("electron")
 const {app} = remote
 const $ = jQuery = require('jquery')
 const log = require('electron-log')
+const swal = require('sweetalert')
+const path = require("path")
+const main = remote.require(path.resolve(__dirname, '../..', 'main.js'))
 
 $(document).ready(() => {
     ipcRenderer.once(`error`, (event, args) => {
         loadPage(args)
     })
+    ipcRenderer.on("settingsClear", (events, args) => {
+        if (args["writeError"]) {
+            log.info(`Renderer: Error writing settings file. Alerting user.`)
+            swal("Whoops!", "We couldn't write to your settings file. This is probably a permissions error.", "error")
+        }
+    })
+})
+
+$("#clearSettings").on('click', () => {
+    main.clearSettings()
 })
 
 function loadPage(type) {
@@ -20,5 +33,7 @@ function loadPage(type) {
         $("#key").css('display', 'block')
     } else if (type === "updateRun") {
         $("#updateRun").css('display', 'block')
+    } else if (type === "parse") {
+        $("#parse").css('display', 'block')
     }
 }
