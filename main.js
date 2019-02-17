42af80795fdc1176204cf056a5c865c9d33433b2
dex <dextronox@users.noreply.github.com>
2019-02-17 21:31:56 +0800

    Undo stupidity

--- main.js
+++ main.js
@@ -832,9 +832,6 @@ exports.clearSettings = () => {
     })
 }
 
-exports.quitRemote = () => {
-    quit()
-}
 function killSwitch(enable) {
     if (enable) {
         fs.readFile(path.join(configDir, 'settings.conf'), 'utf8', (error, data) => {
