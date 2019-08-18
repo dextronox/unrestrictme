0fe59970bf4830ac958a6e3fc8c071066295481f
dex <dextronox@users.noreply.github.com>
2019-08-18 20:01:41 +0800

    Require nativeimage

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //unrestrict.me Desktop Application.
 //Dependencies
-const {app, BrowserWindow, ipcMain, Menu, Tray, dialog, clipboard} = require("electron")
+const {app, BrowserWindow, ipcMain, Menu, Tray, dialog, clipboard, nativeImage} = require("electron")
 const path = require("path")
 module.paths.push(path.resolve('node_modules'));
 module.paths.push(path.resolve('../node_modules'));
