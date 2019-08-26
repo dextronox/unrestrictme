0584dbbb4c33f5f215dedde4600bb5210e50a214
dex <dextronox@users.noreply.github.com>
2019-08-26 21:11:18 +0800

    Revert "Update main.js"
    This reverts commit 888891e1a7873a92e89457ce2dba746b5b56756e.

--- main.js
+++ main.js
@@ -1039,7 +1039,6 @@ function connect(config) {
 }
 
 exports.stealthConnect = (decryptedResponse) => {
-    log.info(os.platform())
     fs.writeFile(path.join(app.getPath("userData"), 'stunnel.conf'), decryptedResponse["stunnel"], (error) => {
         if (error) {
             log.error(`Main: Couldn't write the stunnel configuartion to disk.`)
