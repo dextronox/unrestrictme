73f4f58501bb0e46754c458b252ee8f02fa32f0d
dex <dextronox@users.noreply.github.com>
2020-02-27 20:58:12 +0800

    Update service.js

--- service.js
+++ service.js
@@ -275,8 +275,9 @@ function stealthFunction(wstunnelPath, wstunnelDomain, ovpnConfig, ovpnPath, scr
         }
         console.log(stdout)
         console.log(stderr)
+        if (stdout.includes(`WAIT for datagrames on 127.0.0.1:1194`)) {
+            ovpnFunction(ovpnConfig, ovpnPath, scriptPath)
+        }
     })
-    if (stdout.includes(`WAIT for datagrames on 127.0.0.1:1194`)) {
-        ovpnFunction(ovpnConfig, ovpnPath, scriptPath)
-    }
+
 }
